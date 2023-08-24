8b7815a12e58a0ac1507c81a29091b839aefd53a
MosabShabat <mosabshabat39@gmail.com>
2023-08-24 10:42:40 +0300

    commit 112

--- lib/features/artists/screens/artists_screen.dart
+++ lib/features/artists/screens/artists_screen.dart
@@ -10,7 +10,7 @@ class ArtistsScreen extends StatefulWidget {
   @override
   State<ArtistsScreen> createState() => _ArtistsScreenState();
 }
-
+//mosab shabat
 class _ArtistsScreenState extends State<ArtistsScreen> {
   @override
   Widget build(BuildContext context) {
